79579f11c559e3845ac03a468d9e53451f91ef95
Aron Hrafnsson <aronhr98@gmail.com>
2017-04-08 12:51:33 +0000

    Tillaga að forriti
    Server er console og client er windows form
    
    Það er ekki búið að gera neitt í þessu og kemur error þegar clientinn
    hefur náð tengingu!

---
+++ Forrit/Server/Server/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+
+        private Socket connection;
+        private int counter = 0;
+        private int port = 5000;
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        void Run()
+        {
+            new Thread(RunServer).Start();
+        }
+
+        public void RunServer()
+        {
+            Thread readThread;
+            bool done = false;
+
+            TcpListener listener;
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+
+                while (!done)
+                {
+                    connection = listener.AcceptSocket();
+                    counter++;
+                    readThread = new Thread(GetMessages);
+                    readThread.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+        } // RunServer END
+
+        public void GetMessages()
+        {
+            Socket socket = connection;
+            int count = counter;
+            NetworkStream socketStrem = null;
+            BinaryReader reader = null;
+            BinaryWriter writer = null;
+
+            try
+            {
+                socketStrem = new NetworkStream(socket);
+                reader = new BinaryReader(socketStrem);
+                writer = new BinaryWriter(socketStrem);
+                writer.Write("Connected!");
+                string message = null;
+
+                // do something with messages
+                // Input frá Client koma herna!
+
+            }catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+        }
+    }
+}
---
+++ Forrit/Server_Console/Server_Console/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Server_Console
+{
+    class Program
+    {
+        private Socket connection;
+        private int counter = 0;
+        private int port = 5000;
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        void Run()
+        {
+            new Thread(RunServer).Start();
+        }
+
+        public void RunServer()
+        {
+            Thread readThread;
+            bool done = false;
+
+            TcpListener listener;
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+
+                while (!done)
+                {
+                    connection = listener.AcceptSocket();
+                    counter++;
+                    readThread = new Thread(GetMessages);
+                    readThread.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+        } // RunServer END
+
+        public void GetMessages()
+        {
+            Socket socket = connection;
+            int count = counter;
+            NetworkStream socketStrem = null;
+            BinaryReader reader = null;
+            BinaryWriter writer = null;
+
+            try
+            {
+                socketStrem = new NetworkStream(socket);
+                reader = new BinaryReader(socketStrem);
+                writer = new BinaryWriter(socketStrem);
+                writer.Write("Connected!");
+                string message = null;
+
+                // do something with messages
+                // Input frá Client koma herna!
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+            finally
+            {
+                reader.Close();
+                writer.Close();
+                socketStrem.Close();
+                socket.Close();
+            }
+        }
+    }
+}
+
