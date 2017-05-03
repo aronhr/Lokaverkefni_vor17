f3d86aa31dfd786fd624c8261d57228c1c234012
Adam Bæhrenz Björgvinsson <adambjorgvinsson@gmail.com>
2017-05-03 19:55:08 +0000

    add product helper

---
+++ Forrit/Client/Client/ProductHelper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Client
+{
+    public class ProductHelper
+    {
+        JsonSerializer serializer = new JsonSerializer();
+
+        private T Get<T>(string id = null)
+        {
+            using (WebClient client = new WebClient())
+            {
+                var stream = client.OpenRead("http://localhost:8080/api/product/" + (id == null ? string.Empty : id));
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string s = reader.ReadToEnd();
+                    return JsonConvert.DeserializeObject<T>(s);
+                }
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            return Get<List<Product>>();
+        }
+        public Product GetProduct(string id)
+        {
+            return Get<Product>(id);
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return Name + " " + Price;
+        }
+    }
+}
