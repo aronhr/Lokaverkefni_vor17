d33171e964f17710b551fe71c25277484f75a34e
Aron Hrafnsson <aronhr98@gmail.com>
2017-05-04 18:06:48 +0000

    Tok út kóða sem var ekki i notkun eftir nýja serverinn

--- Forrit/Client/Client/Form1.Designer.cs
+++ Forrit/Client/Client/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -138,11 +137,10 @@
             // 
             // textBox
             // 
-            this.textBox.Enabled = false;
             this.textBox.Location = new System.Drawing.Point(299, 141);
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(193, 20);
-            this.textBox.TabIndex = 9;
+            this.textBox.TabIndex = 0;
             // 
             // buttonEnter
             // 
@@ -196,7 +194,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
             this.ClientSize = new System.Drawing.Size(825, 649);
             this.Controls.Add(this.listBox);
             this.Controls.Add(this.delete);
@@ -213,7 +210,6 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Forrit/Client/Client/Form1.cs
+++ Forrit/Client/Client/Form1.cs
@@ -11,11 +11,6 @@ namespace Client
 {
     public partial class Form1 : Form
     {
-        private NetworkStream output;
-        private BinaryWriter writer;
-        private BinaryReader reader;
-        private string message = "";
-        private int port = 5000;
         private ProductHelper api = new ProductHelper();
 
         public Form1()
@@ -26,55 +21,6 @@ namespace Client
         private void Form1_Load(object sender, EventArgs e)
         {
             GenerateButtons();
-            //Run();
-        }
-
-        void Run()
-        {
-           
-            new Thread(Connect).Start();
-        }
-
-        void Connect()
-        {
-            TcpClient client = null;
-
-            try
-            {
-
-                client = new TcpClient();
-                client.Connect("localhost", port);
-                output = client.GetStream();
-                writer = new BinaryWriter(output);
-                reader = new BinaryReader(output);
-
-                do
-                {
-                    try
-                    {
-                        message = reader.ReadString();
-                        // Uncomment til að byrta message fra server
-                        //MessageBox.Show(message);
-                        if(message == "close")
-                        {
-                            Environment.Exit(Environment.ExitCode);
-                        }
-                    }
-                    catch (Exception error)
-                    {
-                    }
-                } while (message != "close");
-            } catch (Exception ex)
-            {
-                Environment.Exit(Environment.ExitCode);
-            }
-            finally
-            {
-                reader.Close();
-                writer.Close();
-                output.Close();
-                client.Close();
-            }
         }
 
         private void GenerateButtons()
@@ -173,7 +119,7 @@ namespace Client
             Product product = api.GetProduct(productID);
             if (product == null)
             {
-                textBox.Text = "VILLA ???";
+                textBox.Text = "Vara ekki á skrá.";
                 return;
             }
             textBox.Clear();
